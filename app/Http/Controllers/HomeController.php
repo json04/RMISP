da51cab37094c3dff7d8b857046ba7dec46c1ecb
Jayson R. Alanano <json.alanano@gmail.com>
2018-07-19 15:58:18 +0800

    07/19/2018
    Home Controller
    Activity Edit Fix (Temporary)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -47,41 +47,42 @@ class HomeController extends Controller
 
     public function update(Request $request, $id)
     {
-        // $activities = Activity::findOrFail($id);
-        // $activities->reference = $request->Input('reference');
-        // $activities->dateloaded = $request->Input('dateloaded');
-        // $activities->week_ending = $request->Input('we');
-        // $activities->sdt = $request->Input('sdt');
-        // $activities->unitid = $request->Input('unitid');
-        // $activities->groupnumber = $request->Input('groupnumber');
-        // $activities->haulton = $request->Input('haulton');
-        // $activities->driver = $request->Input('driver');
-        // $activities->block = $request->Input('block');
-        // $activities->numberofharvester = $request->Input('numberofharvester');
-        // $activities->rateton = $request->Input('rateton');
-        // $activities->datemilled = $request->Input('datemilled');
-        // $activities->grosstons = $request->Input('grosstons');
-        // $activities->trashpercentage = $request->Input('trashpercentage');
-        // $activities->mill = $request->Input('mill');
-        // $activities->trashtotal = $request->Input('trashtotal');
-        // $activities->nettons = $request->Input('nettons');
-        // $activities->sugar = $request->Input('sugar');
-        // $activities->molases = $request->Input('molases');
-        // $activities->dueharvesters = $request->Input('dueharvesters');
-        // $activities->dueperharvesters = $request->Input('dueperharvesters');
-        // $activities->duedriver = $request->Input('duedriver');
-        // $activities->dueunit = $request->Input('dueunit');
-        // $activities->save();
+        $activities = Activity::findOrFail($id);
+        $activities->reference = $request->Input('reference');
+        $activities->dateloaded = $request->Input('dateloaded');
+        $activities->week_ending = $request->Input('we');
+        $activities->sdt = $request->Input('sdt');
+        $activities->unitid = $request->Input('unitid');
+        $activities->groupnumber = $request->Input('groupnumber');
+        $activities->haulton = $request->Input('haulton');
+        $activities->driver = $request->Input('driver');
+        $activities->block = $request->Input('block');
+        $activities->numberofharvester = $request->Input('numberofharvester');
+        $activities->rateton = $request->Input('rateton');
+        $activities->datemilled = $request->Input('datemilled');
+        $activities->grosstons = $request->Input('grosstons');
+        $activities->trashpercentage = $request->Input('trashpercentage');
+        $activities->mill = $request->Input('mill');
+        $activities->trashtotal = $request->Input('trashtotal');
+        $activities->nettons = $request->Input('nettons');
+        $activities->sugar = $request->Input('sugar');
+        $activities->molases = $request->Input('molases');
+        $activities->dueharvesters = $request->Input('dueharvesters');
+        $activities->dueperharvesters = $request->Input('dueperharvesters');
+        $activities->duedriver = $request->Input('duedriver');
+        $activities->dueunit = $request->Input('dueunit');
+        $activities->save();
 
+        Pivot::where('activities_id', $id)->delete();
         $harvest = $request->Input('harvestersSelect');
         $str = str_replace('"', '', $harvest);
         $dec = json_decode($str[0]);
-        $query = Pivot::whereIn('id', $dec)->where('activities_id', $id)->get();
-        foreach ($query as $value) {     
-            // $storeHarvester->activities_id = $activities->id;
-            // $storeHarvester->week_ending = $request->Input('we');
-            // $storeHarvester->harvesters_id = $value->id;
-            $storeHarvester->save();      
+        foreach ($dec as $value) {     
+            $storeHarvester = new Pivot;
+            $storeHarvester->activities_id = $activities->id;
+            $storeHarvester->week_ending = $request->Input('we');
+            $storeHarvester->harvesters_id = $value;   
+            $storeHarvester->save();   
         }
         
 
